54d1da943064044255f8e0cfac9e7021e8c23fc4
Tino Didriksen <mail@tinodidriksen.com>
2026-02-17 15:34:52 +0000

    WIP local

--- local/01-setup.sh
+++ local/01-setup.sh
@@ -3,6 +3,29 @@ DIR="$(cd $(dirname $0);pwd)"
 USER_UID=$(id -u)
 USER_GID=$(id -g)
 
+if [ ! -s "/etc/apt/sources.list.d/docker.sources" ]; then
+	echo "Installing Docker from https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository - if you don't want this, hit Ctrl-C within 10 seconds"
+	echo ""
+	sleep 10
+
+	sudo apt-get remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+	sudo apt-get -qqy update
+	sudo apt-get -qfy install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+	sudo apt-get -qfy install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
 sudo apt-get -qqy update
 sudo apt-get -qfy install --no-install-recommends composer
 
@@ -17,9 +40,12 @@ pushd tmp
 
 pushd public_html
 	if [ -s "_inc/config.php" ]; then
-		read -p "config.php already exists - next step will rsync and overwrite local changes. Hit CTRL-C to stop here, or enter to continue with sync" SKIP
+		read -p "config.php already exists - next step will rsync and overwrite local changes. Hit Ctrl-C to stop here, or Y to continue: " Y
 	fi
-	rsync -avzHAXx --partial '--exclude=.vscode' manatee@corp2.visl.dk:./public_html/ ./
+	if [ "$Y" != "y" ] && [ "$Y" != "Y" ]; then
+		exit
+	fi
+	rsync -avzHAXx --inplace '--exclude=.vscode' manatee@corp2.visl.dk:./public_html/ ./
 	git pull --all --rebase --autostash
 	composer upgrade
 popd
@@ -29,5 +55,5 @@ pushd storage
 		ssh manatee@corp2.visl.dk 'cd storage/corpora; find . -type d -print0' | xargs -0r mkdir -pv
 		ssh manatee@corp2.visl.dk 'cd storage/corpora; find . -type f -print0' | xargs -0r touch -a
 	popd
-	rsync -avzHAXx --partial '--exclude=cache/**' '--include=*/' '--exclude=*.sqlite' '--exclude=corpora/*/*.*' manatee@corp2.visl.dk:./storage/ ./
+	rsync -avzHAXx --inplace '--exclude=cache/**' '--include=*/' '--exclude=*.sqlite' '--exclude=corpora/*/*.*' manatee@corp2.visl.dk:./storage/ ./
 popd
--- local/03-start-server.sh
+++ local/03-start-server.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 DIR="$(cd $(dirname $0);pwd)"
 
+echo "Starting local server at http://localhost:7080/"
+echo "Open it in a browser to test that the corpus is working as expected. Server can be closed with Ctrl-C"
+echo "Only the locally encoded corpus exists. The rest are skeleton files."
+echo ""
+
 cd "$DIR"
-sudo docker run -it --rm -v "$DIR/tmp:/home/manatee" -p 7080:80 gs-manatee
+sudo docker run -it --rm --name gs-manatee -v "$DIR/tmp:/home/manatee" -p 7080:80 gs-manatee
